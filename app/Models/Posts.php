30008ee2e86aed5bc6aefb86232c58feeded4545
Juan.Fernandes <juan.fernandes@acin.pt>
2022-09-05 15:49:08 +0100

    Initial Setup

---
+++ app/Models/Posts.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Posts extends Model
+{
+    use HasFactory;
+
+    protected $guarded= [];
+    public $timestamps = true;
+/**
+     * Get the user that owns the Posts
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
